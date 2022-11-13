6dd28564b3c486450d6639c4aef8fbe35d8bc1e4
dev <dev@dev.org>
2022-11-13 15:10:44 +0100

    Add project files.

---
+++ ParquetConsole/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Parquet.Data.Rows;
+using Parquet.Data;
+using Parquet;
+using System.Data.Common;
+
+Console.WriteLine("Hello, Parquet!");
+
+// e.g. Metadata
+
+var metaDataTableSchema = new Schema(
+      new DataField<int>("Id"),
+      new DataField<double>("DoubleField"),
+      new DataField<string>("StringField"));
+var metaDataTable = new Table(metaDataTableSchema);
+
+
+
+var random = new Random();
+
+
+
+for (int i = 0; i < 3; i++)
+{
+    metaDataTable.Add(new Row(i, random.NextDouble(), $"StringContent {i}"));
+    Console.WriteLine($"Added meta data: {metaDataTable[i]}");
+
+    var detailDataTable = new Table(
+    new Schema(
+      new DataField<int>("Id"),
+      new DataField<double>("DoubleField"),
+      new DataField<string>("StringField"),
+      new DataField<int>("MetaDataId")));
+
+    
+    for (int j = 0; j < 1000000; j++)
+    {
+        detailDataTable.Add(new Row(j, random.NextDouble(), $"StringContent {j}", i));
+        Console.WriteLine($"Added detail data: {detailDataTable[j]}");
+    }
+
+    using (Stream fileStream = System.IO.File.OpenWrite($"detail_data_{i}.parquet.gzip"))
+    {
+        using var parquetWriter = await ParquetWriter.CreateAsync(detailDataTable.Schema, fileStream);
+        parquetWriter.CompressionMethod = CompressionMethod.Gzip;
+        await parquetWriter.WriteAsync(detailDataTable);
+    }
+}
+
+using (Stream fileStream = System.IO.File.OpenWrite("meta_data_1.parquet.gzip"))
+{
+    using var parquetWriter = await ParquetWriter.CreateAsync(metaDataTableSchema, fileStream);
+    parquetWriter.CompressionMethod = CompressionMethod.Gzip;
+    await parquetWriter.WriteAsync(metaDataTable);
+}
+
+
+
+
+
---
+++ ParquetConsole/create-external-datasource.sql
@@ -0,0 +1,44 @@
+﻿CREATE EXTERNAL DATA SOURCE ParquetStorage
+WITH
+  ( LOCATION = 'adls://<STORAGE_ACCOUNT_NAME>.dfs.core.windows.net/<FOLDER>' ,
+    CREDENTIAL = AzureStorageCredential ,
+  ) ;
+
+CREATE EXTERNAL FILE FORMAT ParquetFileFormat_compressed
+WITH (
+         FORMAT_TYPE = PARQUET,
+		  , DATA_COMPRESSION = 'org.apache.hadoop.io.compress.GzipCodec'
+);
+
+CREATE EXTERNAL TABLE [dbo].[DetailData] (  
+      [Id] int,
+      [DoubleField] float,
+      [StringField] NVARCHAR(MAX),
+	  [MetaDataId] int
+)  
+WITH (LOCATION='/<SUB_FOLDER>/',
+      DATA_SOURCE = ParquetStorage,  
+      FILE_FORMAT = ParquetFileFormat_compressed  
+);
+
+CREATE EXTERNAL TABLE [dbo].[MetaData] (  
+      [Id] int,
+      [DoubleField] float,
+      [StringField] NVARCHAR(MAX),
+	  [NonExistingField] int
+)  
+WITH (LOCATION='/<OTHER_SUB_FOLDER>/',
+      DATA_SOURCE = ParquetStorage,  
+      FILE_FORMAT = ParquetFileFormat_compressed  
+);
+
+/* Example query */
+SELECT 
+	DD.Id,
+	DD.DoubleField,
+	MD.Id,
+	MD.StringField
+FROM [dbo].[MetaData] MD
+INNER JOIN [dbo].[DetailData] DD
+	ON MD.Id = DD.MetaDataId
+		AND MD.Id = 2
